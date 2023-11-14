b7eb596561fc08fa27747bafa1901da50cfbb828
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-11-14 15:09:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const utcDate1 = Date.UTC(
+        parseInt(date1.substring(0, 4)),
+        parseInt(date1.substring(5, 7)) - 1,
+        parseInt(date1.substring(8))
+    );
+
+    const utcDate2 = Date.UTC(
+        parseInt(date2.substring(0, 4)),
+        parseInt(date2.substring(5, 7)) - 1,
+        parseInt(date2.substring(8))
+    );
+
+    // Calculate the difference in milliseconds between the two dates
+    const timeDiff = utcDate2 - utcDate1;
+
+    // Convert the difference to days by dividing by the number of milliseconds in a day
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    return daysDiff;
 };
 
 // Do not change the code below.
